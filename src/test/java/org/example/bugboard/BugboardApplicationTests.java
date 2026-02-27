a8ee80b1d580a4b484003400f8794e4f1cde6fe3
handsome-tiger-789 <tropical7890@gmail.com>
2026-02-27 15:37:27 +0900

    first commit

---
+++ src/test/java/org/example/bugboard/BugboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bugboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
