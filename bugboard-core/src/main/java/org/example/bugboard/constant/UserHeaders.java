301e70b2192ef8bb93964e70ae2e135cbeed4cac
handsome-tiger-789 <tropical7890@gmail.com>
2026-03-07 23:49:17 +0900

    feat(board): Security 설정
    - request.header로 받아올 사용자 정보 core 모듈 정의
    - api 모듈 내 security를 이용해 인증 및 사용자 처리

---
+++ bugboard-core/src/main/java/org/example/bugboard/constant/UserHeaders.java
@@ -0,0 +1,11 @@
+package org.example.bugboard.constant;
+
+public final class UserHeaders {
+
+    public static final String USER_ID = "X-User-Id";
+    public static final String USER_NAME = "X-User-Name";
+    public static final String USER_EMAIL = "X-User-Email";
+
+    private UserHeaders() {
+    }
+}
